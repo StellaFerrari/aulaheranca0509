5a1c85b883efd2c262687458d3297ea67060ae10
Stella <stellafm1205@gmail.com>
2017-09-05 21:45:55 -0300

    Atividade heranca

---
+++ src/UsaCamisa.java
@@ -0,0 +1,14 @@
+
+import com.aula.heranca.camisa.Camisa;
+
+public class UsaCamisa {
+    public static void main(String[] args) {
+        
+        Camisa camisa = new Camisa();
+        System.out.println(camisa.getClass());
+        System.out.println(camisa.getClass().getSimpleName());
+        System.out.println(camisa.hashCode());
+        
+    }
+    
+}
---
+++ src/com/aula/heranca/camisa/Camisa.java
@@ -0,0 +1,9 @@
+package com.aula.heranca.camisa;
+
+public class Camisa {
+    
+    public String marca;
+    public int numero;
+    public double preco;
+    
+}
